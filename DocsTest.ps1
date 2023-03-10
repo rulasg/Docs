987dd51021232cec10c4fddabb4979c8a677c16a
Raúl González <rulasg@outlook.com>
2023-03-10 15:37:09 +0000

    feat: Install TestingHelper if not available

--- DocsTest.ps1
+++ DocsTest.ps1
@@ -1,9 +1,20 @@
 [CmdletBinding()]
 param ()
 
+function Import-TestingHelper($Version){
+    
+    if (-not (import-Module TestingHelper -RequiredVersion $Version -PassThru -ErrorAction SilentlyContinue )) {
+        Install-Module -Name TestingHelper -Force -RequiredVersion $Version
+    }
+    
+    Import-Module -Name TestingHelper -Force -RequiredVersion $Version
+}
+
 $ModuleName = "Docs"
 
-Import-Module -Name TestingHelper -Force
+Import-TestingHelper -Version $TestingHelperRequiredVersion
+
+Import-Module ./Docs.psd1 -Force
 
 # Test-Module -Name $ModuleName -TestName DocsTest_GetFile_Recursive
-Test-Module -Name $ModuleName -TestName DocsTest_Find*
+Test-Module -Name $ModuleName -Verbose
