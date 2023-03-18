a6e34c6c4cc6367c30d2c628b34133c97740f369
Raúl González <rulasg@outlook.com>
2023-03-18 11:19:15 +0000

    feat: generic launch script

---
+++ Test.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding()]
+param ()
+
+function Import-TestingHelper($Version){
+
+    if (-not (import-Module TestingHelper -RequiredVersion $Version -PassThru -ErrorAction SilentlyContinue )) {
+        Install-Module -Name TestingHelper -Force -RequiredVersion $Version
+        Import-Module -Name TestingHelper -Force -RequiredVersion $Version
+    }
+}
+
+Import-TestingHelper -Version $Version
+
+$psd = get-childitem -Path $PSScriptRoot -Filter *.psd1
+
+Import-Module -Name $psd.FullName -Force
+
+Test-Module -Name $psd.BaseName
